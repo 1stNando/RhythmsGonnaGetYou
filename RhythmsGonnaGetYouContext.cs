0131b156c859b184f59829ff6deddb6ed708e5f7
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-01-18 15:54:47 -0500

    Added EF Core library package!

--- RhythmsGonnaGetYouContext.cs
+++ RhythmsGonnaGetYouContext.cs
@@ -1,19 +1,18 @@
 using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
+
 
 namespace RhythmsGonnaGetYou
 {
     class RhythmsGonnaGetYouContext : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
-            optionsBuilder.UseLoggerFactory(loggerFactory);
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+        //     optionsBuilder.UseLoggerFactory(loggerFactory);
 
-            optionsBuilder.UseNpgsql("server=localhost;database=RhythmsGonnaGetYouDB");
-        }
-
-        public DbSet<Band> Bands { get; set; };
+        //     optionsBuilder.UseNpgsql("server=localhost;database=RhythmsGonnaGetYouDB");
     }
+
+    public DbSet<Band> Bands { get; set; };
+}
 }
